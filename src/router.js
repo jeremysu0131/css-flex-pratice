64c02e838f1f4c13780c88daa18f8ba6cf25b540
Jeremy Su <jeremysu0131@gmail.com>
2018-10-04 16:26:54 +0800

    Remove some useless files

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 import Display from "@/components/Display";
 import FlexDirection from "@/components/FlexDirection";
 import JustifyContent from "@/components/JustifyContent";
@@ -19,21 +18,7 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    },
-    {
-      path: "*",
-      component: Display
+      redirect: "display"
     },
     {
       path: "/display",
